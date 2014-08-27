e1f310801e1ffb2b20f3a816c8528c5c728a08b3
kloertm <kloer.he@skywatch.com.tw>
2014-08-27 23:38:53 +0800

    Use the more compatible way to throw the exception.

--- hdrloader.js
+++ hdrloader.js
@@ -4,25 +4,31 @@ module.exports = require('bindings')({
 });
 var convnetjs = require("convnetjs");
 
+var train_kernel_width = 256;
+var train_kernel_height = 256;
+var train_kernel_channel = 3;
+
 function readHDR(hdr_path) {
   var hdr_loader = module.exports.hdrloader();
-  var hdr_data = hdr_loader.loadHDR(hdr_path, 256, 256);
+  var hdr_data = hdr_loader.loadHDR(hdr_path, train_kernel_width, train_kernel_height);
   var hdr_width = hdr_data.readInt32LE(0);
   var hdr_height = hdr_data.readInt32LE(4);
   var hdr_buff = [];
   
-  for (i = 0 ; i < hdr_width * hdr_height * 3 ; ++i) {
+  for (i = 0 ; i < hdr_width * hdr_height * train_kernel_channel ; ++i) {
     hdr_buff.push(hdr_data.readFloatLE(8 + 4 * i));
   }
   return [hdr_width, hdr_height, hdr_buff];
 }
 	
 var layer_defs = [];
-layer_defs.push({type:'input', out_sx:32, out_sy:32, out_depth:3});
+layer_defs.push({type:'input', out_sx:train_kernel_width, out_sy:train_kernel_height, out_depth:train_kernel_channel});
 layer_defs.push({type:'conv', sx:5, filters:8, stride:1, pad:2, activation:'relu'});
 layer_defs.push({type:'pool', sx:2, stride:2});
 layer_defs.push({type:'fc', num_neurons:20, activation:'relu'});
 layer_defs.push({type:'softmax', num_classes:10});
+var net = new convnetjs.Net();
+net.makeLayers(layer_defs);
 
 var myHDR = readHDR('flower.hdr');
 console.log(myHDR);
