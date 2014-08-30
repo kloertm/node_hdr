4b11538819b440a9c6fc56c7299adaf736eaa8e0
kloertm <kloer.tm@gmail.com>
2014-08-30 14:21:25 +0800

    Code refactor.

--- hdrloader.js
+++ hdrloader.js
@@ -5,7 +5,7 @@ module.exports = require('bindings')({
 var convnetjs = require("convnetjs");
 var fs = require('fs');
 
-var train_iteration = 32;
+var train_iteration = 64;
 var train_kernel_width = 128;
 var train_kernel_height = 128;
 var train_kernel_channel = 3;
@@ -42,9 +42,9 @@ function getHDRs(hdr_dir, recursive) {
   return results;
 }
 
-function generateNet(kernel_width, kernel_height, kernel_channel) {
+function generateNet() {
   var layer_defs = [];
-  layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:kernel_width*kernel_height*kernel_channel});
+  layer_defs.push({type:'input', out_sx:train_kernel_width, out_sy:train_kernel_height, out_depth:train_kernel_channel});
   layer_defs.push({type:'fc', num_neurons:5, activation:'sigmoid'});
   layer_defs.push({type:'regression', num_neurons:1});
   var net = new convnetjs.Net();
@@ -57,7 +57,7 @@ function fillVol(raw_data) {
    return vol;
 }
 
-function makeDataset(hdr_dir, kernel_width, kernel_height, kernel_channel) {
+function makeDataset(hdr_dir) {
   var training_list = getHDRs(hdr_dir, false);
   var data = [];
   var labels = [];
@@ -79,9 +79,9 @@ function makeDataset(hdr_dir, kernel_width, kernel_height, kernel_channel) {
 }
 
 function trainHDRs(hdr_dir) {
-  var net = generateNet(train_kernel_width, train_kernel_height, train_kernel_channel);
+  var net = generateNet();
   var trainer = new convnetjs.Trainer(net, {method: 'sgd', learning_rate: 0.01, l2_decay: 0.001, momentum: 0.9, batch_size: 1, l1_decay: 0.001});
-  var dataset = makeDataset(hdr_dir, train_kernel_width, train_kernel_height, train_kernel_channel);
+  var dataset = makeDataset(hdr_dir);
   var avloss = 0.0;
   for (var iteration = 0 ; iteration < train_iteration ; iteration++) {
     for (var data_set_index in dataset.data) {
@@ -93,7 +93,7 @@ function trainHDRs(hdr_dir) {
 }
 
 function testHDRs(hdr_dir, trained_net) {
-  var dataset = makeDataset(hdr_dir, train_kernel_width, train_kernel_height, train_kernel_channel);
+  var dataset = makeDataset(hdr_dir);
   for (var data_set_index in dataset.data) {
     var prediction = trained_net.forward(dataset.data[data_set_index]);
     console.log('prediction is : ' + prediction.w[0]);
